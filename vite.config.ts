3ae7cef3ccf596aec8b10311a9ea4469b5be19ea
rosch-arthur <rosch.arthur@gmail.com>
2024-12-11 03:36:18 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/player.tsx',  // Ponto de entrada do seu player
-      name: 'VideoPlayer',      // Nome global para exportação
-      fileName: 'player',       // Nome do arquivo de saída
-      formats: ['umd', 'es'],   // Formatos compatíveis com o navegador
+      entry: 'src/player.tsx',
+      name: 'VideoPlayer',
+      fileName: 'player', // Aqui você pode escolher o nome do arquivo
+      formats: ['umd'],   // Gerar formato UMD, que é mais compatível com navegadores
     },
     rollupOptions: {
       external: ['react', 'react-dom'], // Excluindo dependências do pacote
