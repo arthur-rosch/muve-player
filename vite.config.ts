441128b58df43363d196a05b84653c4621dec8d0
rosch-arthur <rosch.arthur@gmail.com>
2024-12-11 03:45:29 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
+  plugins: [react()],
 });
