3011deb84f3a273cfd628fd57cfd01e7efda8ad6
rosch-arthur <rosch.arthur@gmail.com>
2024-12-11 03:25:10 -0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,19 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/player.tsx', // Entrada para o player
-      name: 'VideoPlayer', // Nome global da biblioteca para acesso no navegador
-      fileName: 'player', // Nome do arquivo de saída
-      formats: ['es', 'umd'], // Gerar tanto em formato ESM quanto UMD
-    },
+    entry: 'src/player.tsx',
+    name: 'VideoPlayer',
+    fileName: 'player.umd',  // Geração do arquivo player.umd.js
+    formats: ['umd'],
+  },
     rollupOptions: {
-      external: ['react', 'react-dom'], // Excluir react e react-dom do pacote final
+      external: ['react', 'react-dom'], // Excluindo dependências do pacote
       output: {
         globals: {
-          react: 'React', // Definir o React como global
-          'react-dom': 'ReactDOM', // Definir o ReactDOM como global
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-  },
-  define: {
-    // Definir a variável process.env.NODE_ENV para produção, já que é a versão de build
-    'process.env.NODE_ENV': JSON.stringify('production'),
   },
 });
