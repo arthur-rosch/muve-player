f86ade732e835da06c36eaf82b01c4ca1f78ce9d
rosch-arthur <rosch.arthur@gmail.com>
2024-12-11 03:43:20 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: 'src/player.tsx',
-      name: 'VideoPlayer',
-      fileName: 'player',  // Usando o nome do arquivo como 'player.js'
-    },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
